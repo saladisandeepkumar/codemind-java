34b9b20cbbab73fbb3a98edc99ace82c3feb9462
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-01-24 20:41:23 +0530

    import java.io.*; import java.util.*; public class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         switch(ch)         {             case 'V','v':                 System.out.println("Violet");                 break;             case 'I','i':                 System.out.println("Indigo");                 break;             case 'B','b':                 System.out.println("Blue");                 break;             case 'G','g':                 System.out.println("Green");                 break;             case 'Y','y':                 System.out.println("Yellow");                 break;             case 'O','o':                 System.out.println("Orange");                 break;             case 'R','r':                 System.out.println("Red");                 break;             default:             System.out.println("-1");
            }
        }
    }

---
+++ Color_Code.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+public class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        switch(ch)
+        {
+            case 'V','v':
+                System.out.println("Violet");
+                break;
+            case 'I','i':
+                System.out.println("Indigo");
+                break;
+            case 'B','b':
+                System.out.println("Blue");
+                break;
+            case 'G','g':
+                System.out.println("Green");
+                break;
+            case 'Y','y':
+                System.out.println("Yellow");
+                break;
+            case 'O','o':
+                System.out.println("Orange");
+                break;
+            case 'R','r':
+                System.out.println("Red");
+                break;
+            default:
+            System.out.println("-1");
+                
+        }
+    }
+}
+           
