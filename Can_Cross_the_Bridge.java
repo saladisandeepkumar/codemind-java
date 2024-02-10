a0e8068fb4b86d6488cf2f9eff1288d7e2e0cf7f
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-02-10 10:59:47 +0530

    import java.util.*; public class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int sum=(z-y)/x;         System.out.println(sum);     } }

---
+++ Can_Cross_the_Bridge.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int sum=(z-y)/x;
+        System.out.println(sum);
+    }
+}
