2b7c48f0eaab050fe146cb86c21ff1337a08b241
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-02-14 10:24:48 +0530

    import java.util.*; public class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         for(int i=n;i<=m;i++)         {             System.out.print(i +" ");         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        for(int i=n;i<=m;i++)
+        {
+            System.out.print(i +" ");
+        }
+    }
+}
