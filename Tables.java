77043d2743b8c269d8a823ab905600ca1b01f93d
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-02-21 11:06:56 +0530

    import java.util.*; public class frames{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=1;i<=b;i++)         {         if(i%2!=0)         System.out.println(a+" x "+i+" = "+a*i);         }     } }

---
+++ Tables.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class frames{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=1;i<=b;i++)
+        {
+        if(i%2!=0)
+        System.out.println(a+" x "+i+" = "+a*i);
+        }
+    }
+}
