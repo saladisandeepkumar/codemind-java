c1fd2388e551dbb311be3fbe1fd87f81ebbfd6d9
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-03-06 11:22:10 +0530

    import java.util.*; public class main{ public static void main(String args[]) {     Scanner sc=new Scanner(System.in);     int n=sc.nextInt();     int count=0;     for(int i=1;i<=n;i++)     {         if(n%i==0)         {             count+=1;         }     }     if(count==2)     {     System.out.println("prime");     }     else     {         System.out.println("not a prime");     } } }

---
+++ prime_number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class main{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int count=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count+=1;
+        }
+    }
+    if(count==2)
+    {
+    System.out.println("prime");
+    }
+    else
+    {
+        System.out.println("not a prime");
+    }
+}
+}
