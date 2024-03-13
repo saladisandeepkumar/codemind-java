f17bdec0563e1c3ebcd7f37b9aa4a18732f3bd15
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-03-13 11:09:21 +0530

    import java.util.*; public class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int s=sc.nextInt();         int sum=(int)Math.pow(n,m);         int mod=sum%s;         System.out.println(mod);     }
    }

---
+++ Power_of_a_Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int s=sc.nextInt();
+        int sum=(int)Math.pow(n,m);
+        int mod=sum%s;
+        System.out.println(mod);
+    }
+    
+}
