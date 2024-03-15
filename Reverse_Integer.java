3e60f8032fc9c4a6f78153a137935931e93caaf3
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-03-15 21:22:16 +0530

    import java.util.*; public class main{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int digit;         int sum=0;         while(n!=0)         {             digit=n%10;             sum=sum*10+digit;             n=n/10;         }         System.out.println(sum);     } }

---
+++ Reverse_Integer.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int digit;
+        int sum=0;
+        while(n!=0)
+        {
+            digit=n%10;
+            sum=sum*10+digit;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
